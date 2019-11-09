c3a65aa974a09df753115df737445618fc12a072
Rahul Radhakrishnan <rahulrkr@hotmail.com>
2019-11-10 01:04:49 +0530

    Updated configurations

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { execSync } = require('child_process');
 const core = require('@actions/core');
 
-const AWS_ACCESS_KEY_ID = core.getInput('AWS_ACCESS_KEY_ID', { required: true });
-const AWS_SECRET_ACCESS_KEY = core.getInput('AWS_SECRET_ACCESS_KEY', { required: true });
+const aws_access_key = core.getInput('aws_access_key', { required: true });
+const aws_secret_access_key = core.getInput('aws_secret_access_key', { required: true });
 
-const awsRegion = core.getInput('AWS_REGION') || process.env.AWS_DEFAULT_REGION || 'us-east-1';
+const aws_region = core.getInput('aws_region') || process.env.AWS_DEFAULT_REGION || 'us-east-1';
 const direction = core.getInput('direction') || 'push';
-const IMAGE = core.getInput('image', { required: true });
+const image = core.getInput('image', { required: true });
 
 function run(cmd, options = {}) {
     if (!options.hide) {
@@ -17,13 +17,13 @@ function run(cmd, options = {}) {
         encoding: 'utf-8',
         env: {
             ...process.env,
-            AWS_ACCESS_KEY_ID,
-            AWS_SECRET_ACCESS_KEY,
-        },
+            AWS_ACCESS_KEY_ID: aws_access_key,
+            AWS_SECRET_ACCESS_KEY: aws_secret_access_key,
+        }
     });
 }
 
-run(`$(aws ecr get-login --no-include-email --region ${awsRegion})`);
+run(`$(aws ecr get-login --no-include-email --region ${aws_region})`);
 
 const accountData = run(`aws sts get-caller-identity --output json`);
 const awsAccountId = JSON.parse(accountData).Account;
@@ -31,9 +31,9 @@ const awsAccountId = JSON.parse(accountData).Account;
 core.setOutput('account', awsAccountId);
 
 if (direction === 'push') {
-    console.log(`Pushing image ${IMAGE} to ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${IMAGE}`);
-    run(`docker tag ${IMAGE} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${IMAGE}`);
-    run(`docker push ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${IMAGE}`);
+    console.log(`Pushing image ${image} to ${awsAccountId}.dkr.ecr.${aws_region}.amazonaws.com/${image}`);
+    run(`docker tag ${image} ${awsAccountId}.dkr.ecr.${aws_region}.amazonaws.com/${image}`);
+    run(`docker push ${awsAccountId}.dkr.ecr.${aws_region}.amazonaws.com/${image}`);
 } else {
     throw new Error(`Unknown direction ${direction}`);
 }
